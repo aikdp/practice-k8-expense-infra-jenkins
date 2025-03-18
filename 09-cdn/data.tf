97d04ba62f81067d2cb44ab6c2de2604dd237f0d
aikdp <kdprasad028@gmail.com>
2025-03-18 12:18:42 +0530

    jenkins expense infra practice

--- 09-cdn/data.tf
+++ 09-cdn/data.tf
@@ -9,6 +9,6 @@ data "aws_cloudfront_cache_policy" "cacheOptimized" {
 
 
 #Use SSM parameter to store in aws and get the info using data source
-data "aws_ssm_parameter" "https_certificate_arn" {
-  name = "/${var.project_name}/${var.environment}/https_certificate_arn"
+data "aws_ssm_parameter" "acm_https_cert_arn" {
+  name = "/${var.project_name}/${var.environment}/acm_https_cert_arn"
 }
