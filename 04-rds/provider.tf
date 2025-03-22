0d405392cf9e5b7fe16dd21454be83461ce2d47e
aikdp <kdprasad028@gmail.com>
2025-03-22 17:11:59 +0530

    Jenkins practice

--- 04-rds/provider.tf
+++ 04-rds/provider.tf
@@ -4,6 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       # version = "~> 5.0"
       version = "5.92.0"
+      
     }
   }
   backend "s3" { #need to create manually in AWS, then use here
